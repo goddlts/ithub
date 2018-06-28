46b2b4afb19aeadcad57d8b66a5e8462591082d5
goddlts <54337467@qq.com>
2018-06-28 14:59:15 +0800

    配置静态资源和模板引擎

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 // 负责：1 配置  2. 监听端口
 
 const express = require('express');
+// 导入处理模板的模块
+const expressArtTemplate = require('express-art-template');
 // 导入路由模块
 const router = require('./routes/router');
 const app = express();
@@ -13,6 +15,12 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log('监听 3000');
 });
+
+// 处理静态资源  -- 下载bootstrap@3.3.7  jquery
+app.use('/public', express.static('./public'));
+app.use('/node_modules', express.static('./node_modules'));
+// 配置模板引擎
+app.engine('html', expressArtTemplate);
 
 
 // 注册路由
--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
 exports.showIndex = (req, res) => {
-  res.send('showIndex');
+  // res.send('showIndex');
+  res.render('index.html');
 };
