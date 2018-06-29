6b563b46b81ac47ff96f69467b2141a86a16e071
goddlts <54337467@qq.com>
2018-06-29 16:22:17 +0800

    添加话题的接口

--- controllers/topic.js
+++ controllers/topic.js
@@ -1,4 +1,5 @@
 const categoryModel = require('../models/category');
+const topicModel = require('../models/topic');
 
 // 1 显示添加话题的页面
 exports.showCreate = (req, res) => {
@@ -8,9 +9,38 @@ exports.showCreate = (req, res) => {
     });
   });
 };
+// 2 处理添加话题  -- 接口
 exports.handleCreate = (req, res) => {
-  res.send('handleCreate');
+  if (!req.session.user) {
+    res.json({
+      code: 403,
+      msg: '登录过期，请先登录'
+    });
+  }
+  // TODO   验证用户输入
+  req.body.userId = req.session.user.id;
+  req.body.createdAt = new Date();
+  topicModel.createTopic(req.body, (err, isOK) => {
+    if (err) {
+      return res.json({
+        code: 500,
+        msg: '服务器内部错误'
+      });
+    }
+    if (isOK) {
+      res.json({
+        code: 200,
+        msg: '添加话题成功'
+      });
+    } else {
+      res.json({
+        code: 400,
+        msg: '话题添加失败'
+      });
+    }
+  });
 };
+
 exports.showTopic = (req, res) => {
   res.send('showTopic');
 };
