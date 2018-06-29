bbdb5dc7279a6b50ac80e5d455d8f166a0e1bfe2
goddlts <54337467@qq.com>
2018-06-29 15:06:41 +0800

    把session存储到mysql

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const expressArtTemplate = require('express-art-template');
 const router = require('./routes/router');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 const app = express();
 
 const PORT = 3000;
@@ -31,11 +32,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // { "name": "zs", "age": 18 }
 // app.use(bodyParser.json());
 
+// 把session保存到mysql中
+var options = {
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'root',
+  database: 'ithub'
+};
+
+var sessionStore = new MySQLStore(options);
 // 配置session
 app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
+  key: 'sessionid',  // 修改sessionid的名称
+  secret: 'keyboard cat',  // 对sessionid 进行加密 
+  resave: false,   // 强制重新存储服务器上的session数据  
+  store: sessionStore,   // 配置把session数据存储到mysql
+  saveUninitialized: true  // 即使不写session 也会生成sessionid
 }));
 
 
